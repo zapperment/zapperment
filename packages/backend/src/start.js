d5c3df22c7df45627f7a779a7dc30f7670cfed35
Roman Tayupov <rtayupov@ebay.com>
2019-05-06 16:36:48 +0200

    add mongodb client

--- packages/backend/src/start.js
+++ packages/backend/src/start.js
@@ -1,29 +1,33 @@
-const http = require("http");
-const openSocket = require("socket.io");
-const path = require("path");
-const { Worker } = require("worker_threads");
-const { START_PLAYING } = require("./constants");
-const { BEAT } = require("@zapperment/shared");
-const { initialTempo, port } = require("./config");
+const db = require('./db');
+const http = require('http');
+const openSocket = require('socket.io');
+const path = require('path');
+const { Worker } = require('worker_threads');
+const { START_PLAYING } = require('./constants');
+const { BEAT } = require('@zapperment/shared');
+const { initialTempo, port } = require('./config');
 
 module.exports = () => {
   const app = http.createServer();
   const io = openSocket(app);
-  io.set("origins", "*:*");
-  const midiBeat = new Worker(path.join(__dirname, "./midiBeatWorker.js"), {
-    workerData: { tempo: initialTempo }
+  io.set('origins', '*:*');
+  const midiBeat = new Worker(path.join(__dirname, './midiBeatWorker.js'), {
+    workerData: { tempo: initialTempo },
   });
-
-  midiBeat.postMessage(START_PLAYING);
-
-  app.listen(port);
-
-  midiBeat.on("message", message => {
+  midiBeat.on('message', message => {
     switch (message) {
       case BEAT:
-        io.emit(BEAT, { for: "everyone" });
+        io.emit(BEAT, { for: 'everyone' });
         break;
       default:
     }
+  });
+
+  db.init(err => {
+    if (err) {
+      throw err;
+    }
+    midiBeat.postMessage(START_PLAYING);
+    app.listen(port);
   });
 };
