7b151a3e9d101754235f3189d4db6353816625fd
Roman Tayupov <rtayupov@ebay.com>
2019-05-07 11:45:34 +0200

    update scenes and store them in mongo

--- packages/backend/src/loop.js
+++ packages/backend/src/loop.js
@@ -1,11 +1,47 @@
 const loop = {
   scene: {
-    current: {},
-    previous: {},
+    current: {
+      mixer: [
+        { channel: 1, mute: true },
+        { channel: 2, mute: true },
+        { channel: 3, mute: true },
+        { channel: 4, mute: true },
+        { channel: 5, mute: true },
+        { channel: 6, mute: true },
+        { channel: 7, mute: true },
+        { channel: 8, mute: true },
+        { channel: 9, mute: true },
+      ],
+    },
+    previous: {
+      mixer: [
+        { channel: 1, mute: true },
+        { channel: 2, mute: true },
+        { channel: 3, mute: true },
+        { channel: 4, mute: true },
+        { channel: 5, mute: true },
+        { channel: 6, mute: true },
+        { channel: 7, mute: true },
+        { channel: 8, mute: true },
+        { channel: 9, mute: true },
+      ],
+    },
   },
   stats: {
     claps: 0,
   },
 };
 
-module.exports = loop;
+const updateScene = async scene => {
+  console.log('NEW SCENE')
+  const { db } = require('./db');
+  await db.collection('loops').insertOne(loop);
+
+  loop.scene.previous = loop.scene.current;
+  loop.scene.current = scene;
+};
+
+module.exports = {
+  loop,
+  updateScene,
+};
--- packages/backend/src/socket/index.js
+++ packages/backend/src/socket/index.js
@@ -1,7 +1,7 @@
 const openSocket = require('socket.io');
 const { STATS_CLAP, NEW_SCENE } = require('@zapperment/shared');
 
-const loop = require('../loop');
+const { loop } = require('../loop');
 
 module.exports = {
   configure: app => {
@@ -10,9 +10,9 @@ module.exports = {
 
     io.on('connection', socket => {
       socket.on(STATS_CLAP, () => {
-				loop.stats.claps++;
+        loop.stats.claps++;
       });
-		});
+    });
     return io;
   },
 };
