b0850f7e6f8627e6722dd8532982e265a3d18724
Patrick Hund <pahund@team.mobile.de>
2019-05-07 14:13:27 +0200

    added bonanza bassline
    Change-Id: Idaeae4f12c34eb6016172862e6a2449595d048fc

--- packages/backend/src/MidiController.js
+++ packages/backend/src/MidiController.js
@@ -5,11 +5,24 @@ class MidiController {
     this.midiOut = midiOut;
   }
 
+  send(channel, controller, value) {
+    this.midiOut.send(jzz.MIDI.control(channel, controller, value));
+  }
+
   changeScene(scene) {
+    // mixer
     for (const { channel, muted } of scene.mixer) {
-      this.midiOut.send(jzz.MIDI.control(0, channel + 101, muted ? 127 : 0));
+      this.send(0, channel + 101, muted ? 127 : 0);
     }
-    this.midiOut.send(jzz.MIDI.control(1, scene.percussion.pattern + 101, 127))
+    // percussion
+    this.send(1, scene.percussion.pattern + 101, 127);
+    // bonanza bassline
+    this.send(2, 102, scene.bonanza.pulsarLevel);
+    this.send(2, 103, scene.bonanza.filter);
+    this.send(2, 104, scene.bonanza.filterEnv);
+    this.send(2, 105, scene.bonanza.lowCut ? 127 : 0);
+    this.send(2, 106, scene.bonanza.rateOneEighth ? 127 : 0);
+    this.send(2, 107, scene.bonanza.sawSolo ? 127 : 0);
   }
 }
 
