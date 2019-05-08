478a57b5a6c2c077237cbc3d1c81ab34ae50286b
Patrick Hund <pahund@team.mobile.de>
2019-05-08 09:35:13 +0200

    changed app title in client
    Change-Id: Iaecf12c071eb845831b279fd9552f3cf13053f36

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client';
-import Lamp from './Lamp';
+import React, { Component } from "react";
+import io from "socket.io-client";
+import Lamp from "./Lamp";
 import ClapButton from "./ClapButton";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
-const { protocol, hostname} = window.location;
+const { protocol, hostname } = window.location;
 const serverUrl = `${protocol}//${hostname}:3001`;
 const socket = io(serverUrl);
 
@@ -13,13 +13,17 @@ class App extends Component {
   render() {
     return (
       <div className={styles.component}>
-        <audio src={`${serverUrl}/stream.wav`} />
         <Lamp socket={socket} />
 
         <div className={styles.actions}>
           <ClapButton socket={socket} />
         </div>
-
+        <div className={styles.controls}>
+          <audio controls>
+            <source src={`${serverUrl}/stream.mp3`} type="audio/mpeg" />
+            <p>Oops – your browser doesn't support HTML5 audio!</p>
+          </audio>
+        </div>
       </div>
     );
   }
