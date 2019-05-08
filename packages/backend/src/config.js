f6ccd3f0947b5dff7862ea5e327976dbc9abcce9
Roman Tayupov <rtayupov@ebay.com>
2019-05-08 14:22:24 +0200

    random emoji sizes

--- packages/backend/src/config.js
+++ packages/backend/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  midiPortName: "IAC Driver Bus 1",
+  midiPortName: "IAC Driver IAC Bus 1",
   initialTempo: 105,
   barsPerLoop: 8,
   port: 3001
--- packages/frontend/src/Emoji.js
+++ packages/frontend/src/Emoji.js
@@ -4,12 +4,16 @@ import './Emoji.css';
 export default ({ icon }) => {
   const defaultPosition = `${Math.floor(Math.random() * 100) + 1}vw`;
   const [horizontalPosition] = useState(defaultPosition);
+
+  const defaultSize = `${Math.floor(Math.random() * 3) + 2}vw`;
+  const [fontSize] = useState(defaultSize);
+
   return (
     <span
       className="emoji"
       role="img"
       aria-label="emoji"
-      style={{ left: horizontalPosition }}
+      style={{ fontSize, left: horizontalPosition }}
     >
       {icon}
     </span>
