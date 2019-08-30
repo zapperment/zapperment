e6a8f0da489d6e32d9a50a2bee2072e9f4d93535
Patrick Hund <pahund@team.mobile.de>
2019-08-30 09:31:18 +0200

    Work in progress, attempt to stream Reason through socket.io and audio context
    Change-Id: I2e107e7dca9514f611bfb8295b1f16b5e881a71e

--- packages/backend/src/socket/index.js
+++ packages/backend/src/socket/index.js
@@ -1,30 +1,38 @@
-const openSocket = require('socket.io');
+const createIo = require("socket.io");
+const ss = require("socket.io-stream");
+const audio = require("osx-audio");
+
 const {
   STATS_NEW_CLAP,
   STATS_NEW_SHARED_CLAPS,
   STATS_NEW_BOO,
-  STATS_NEW_SHARED_BOOS,
-} = require('@zapperment/shared');
+  STATS_NEW_SHARED_BOOS
+} = require("@zapperment/shared");
 
 const { loop } = require("../model/loop");
 
+const audioInput = new audio.Input();
+
 module.exports = {
   configure: app => {
-    const io = openSocket(app);
-    io.set('origins', '*:*');
+    const io = createIo(app);
+    io.set("origins", "*:*");
 
-    io.on('connection', socket => {
+    io.on("connection", socket => {
+      const stream = ss.createStream();
+      ss(socket).emit("audio-stream", stream);
+      audioInput.pipe(stream);
       socket.on(STATS_NEW_CLAP, () => {
         console.log("CLAPPED!");
         loop.stats.claps++;
-        io.emit(STATS_NEW_SHARED_CLAPS, loop.stats.claps, { for: 'everyone' });
+        io.emit(STATS_NEW_SHARED_CLAPS, loop.stats.claps, { for: "everyone" });
       });
       socket.on(STATS_NEW_BOO, () => {
         console.log("BOO'D!");
         loop.stats.boos++;
-        io.emit(STATS_NEW_SHARED_BOOS, loop.stats.boos, { for: 'everyone' });
+        io.emit(STATS_NEW_SHARED_BOOS, loop.stats.boos, { for: "everyone" });
       });
     });
     return io;
-  },
+  }
 };
--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client';
-import Lamp from './Lamp';
-import ClapButton from './ClapButton';
-import Emoji from './Emoji';
+import React, { Component } from "react";
+import io from "socket.io-client";
+import ss from "socket.io-stream";
+import Lamp from "./Lamp";
+import ClapButton from "./ClapButton";
+import Emoji from "./Emoji";
 
-import styles from './App.module.css';
-import BooButton from './BooButton';
+import styles from "./App.module.css";
+import BooButton from "./BooButton";
 
 const { protocol, hostname } = window.location;
 const serverUrl = `${protocol}//${hostname}:3001`;
 const socket = io(serverUrl);
+const soundController = {
+  nextTime: 0,
+  speakerContext: new AudioContext(),
+  playCache(cache) {
+    while (cache.length) {
+      const source = soundController.speakerContext.createBufferSource();
+      source.buffer = cache.shift();
+      source.connect(soundController.speakerContext.destination);
+      if (soundController.nextTime === 0) {
+        // add a delay of 0.05 seconds
+        soundController.nextTime =
+          soundController.speakerContext.currentTime + 0.05;
+      }
+      source.start(soundController.nextTime);
+      // schedule buffers to be played consecutively
+      soundController.nextTime += source.buffer.duration;
+    }
+  }
+};
 
 class App extends Component {
   state = { claps: 0, boos: 0 };
@@ -22,35 +42,54 @@ class App extends Component {
     this.setState({ boos });
   };
 
+  componentDidMount() {
+    ss(socket).on("audio-stream", stream => {
+      soundController.nextTime = 0;
+      let init = false;
+      const audioCache = [];
+      console.log("receiving audio stream");
+      stream.on("data", data => {
+        const array = new Float32Array(data);
+        // const array = Float32Array.from(data);
+        const buffer = soundController.speakerContext.createBuffer(
+          1,
+          2048,
+          44100
+        );
+        buffer.copyToChannel(array, 0);
+        audioCache.push(buffer);
+        if (init || audioCache.length > 5) {
+          init = true;
+          soundController.playCache(audioCache);
+        }
+      });
+      stream.on("end", () => console.log("end of audio stream"));
+    });
+  }
+
   render() {
     const { claps, boos } = this.state;
     return (
       <div className={styles.component}>
         <div className="claps">
           {[...new Array(claps)].map((e, i) => (
-            <Emoji key={i} icon={'👏'}/>
+            <Emoji key={i} icon={"👏"} />
           ))}
         </div>
 
         <div className="boos">
           {[...new Array(boos)].map((e, i) => (
-            <Emoji key={i} icon={'💩'}/>
+            <Emoji key={i} icon={"💩"} />
           ))}
         </div>
 
         <div className={styles.logo}>
-          <img src="./zapperment-logo.png"/>
+          <img src="./zapperment-logo.png" />
         </div>
         <Lamp socket={socket} />
         <div className={styles.actions}>
           <ClapButton socket={socket} onClaps={this.handleClaps} />
           <BooButton socket={socket} onBoos={this.handleBoos} />
-        </div>
-        <div className={styles.controls}>
-          <audio controls autoPlay preload="none">
-            <source src={`${serverUrl}/stream.mp3`} type="audio/mpeg" />
-            <p>Oops – your browser doesn't support HTML5 audio!</p>
-          </audio>
         </div>
       </div>
     );
