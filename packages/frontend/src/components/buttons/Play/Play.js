7a82d9336f4fed9f11d30696186f40517c41af62
Patrick Hund <pahund@team.mobile.de>
2019-09-17 18:13:19 +0200

    FFmpeg and Icecast (#30)
    * updated zapperment logo illustrator file
    * using icecast server and ffmpeg to stream audio from Reason
    * Using Amplitude.js instead of Howl.js to play audio stream in the frontend
    * Zapperment backend is not responsible anymore to stream audio

--- packages/frontend/src/components/buttons/Play/Play.js
+++ packages/frontend/src/components/buttons/Play/Play.js
@@ -2,22 +2,14 @@ import React from "react";
 import cx from "classnames";
 import icon from "./play.svg";
 import styles from "./Play.module.css";
-import { Howl } from "howler";
-import { getServerUrl } from "../../../utils";
 
 export default ({ onPlay }) => {
   const handleClick = () => {
     console.log("play!");
-    const howl = new Howl({
-      src: `${getServerUrl()}/stream.mp3`,
-      html5: true,
-      format: ["mp3", "aac"]
-    });
-    howl.play();
     onPlay();
   };
   return (
-    <button className={cx(styles.component)} onClick={handleClick}>
+    <button className={cx(styles.component, "amplitude-play-pause")} onClick={handleClick}>
       <img src={icon} alt="play" className={styles.icon} />
       <span className={styles.label}>Play!</span>
     </button>
--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import Amplitude from "amplitudejs";
+import { getIcecastUrl } from "./utils";
 
 const vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+Amplitude.init({
+  songs: [
+    {
+      url: `${getIcecastUrl()}/stream.ogg`,
+      live: true
+    }
+  ],
+  preload: "none"
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
