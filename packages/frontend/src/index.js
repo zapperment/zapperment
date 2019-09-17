9b8456e92dcba004f85332552cada6cbc801fe70
Patrick Hund <pahund@team.mobile.de>
2019-09-17 18:06:47 +0200

    (squash me)
    Change-Id: Ida3549d94aca52a77fadc8959ed18c4d3ae26b29

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import Amplitude from "amplitudejs";
-import{getIcecastUrl}from'./utils';
+import { getIcecastUrl } from "./utils";
 
 const vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -18,7 +18,7 @@ Amplitude.init({
       live: true
     }
   ],
-  preload: 'none'
+  preload: "none"
 });
 
 // If you want your app to work offline and load faster, you can change
