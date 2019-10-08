bb624b140372ed3e765b37d7310fc1f6b002fea1
Patrick Hund <pahund@team.mobile.de>
2019-10-08 18:43:43 +0200

    * disabled Babel transpilation for backend unit tests (workaround   for Jest issue)
    Change-Id: I621f3d751b1be109cbcead4b7cfd643f971bae5f

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   testEnvironment: "node",
-  setupFiles: ["./jest.setup.js"]
+  setupFiles: ["./jest.setup.js"],
+  // disable Babel completely, we don't need it
+  transformIgnorePatterns: ['.*']
 };
