d12406023b3116873ea397186e746af1ba6d50dd
Patrick Hund <pahund@team.mobile.de>
2019-10-21 09:37:51 +0200

    Issue #46: added check to have at least one channel always unmuted in function to build random scene to avoid total silence
    Change-Id: Id65ad3f91748c36f9a3e2934ae82ec6dea040c40

--- packages/backend/src/model/build/buildRandomScene.js
+++ packages/backend/src/model/build/buildRandomScene.js
@@ -1,19 +1,35 @@
 const { getRandomBool } = require("./utils");
 
 module.exports = () => ({
-  mixer: [
-    // 102 piano
-    { channel: 1, muted: getRandomBool() },
-    // 103 bottle perc
-    { channel: 2, muted: getRandomBool() },
-    // 104 dirty beat
-    { channel: 3, muted: getRandomBool() },
-    // 105 fuzz bass
-    { channel: 4, muted: getRandomBool() },
-    // 106 fm arp
-    { channel: 5, muted: getRandomBool() }
-  ],
+  mixer: buildMixer(),
   nervomat: {
     annoyMe: getRandomBool()
   }
 });
+
+function buildMixer() {
+  let mixer;
+  do {
+    mixer = [
+      // 102 piano
+      { channel: 1, muted: getRandomBool() },
+      // 103 bottle perc
+      { channel: 2, muted: getRandomBool() },
+      // 104 dirty beat
+      { channel: 3, muted: getRandomBool() },
+      // 105 fuzz bass
+      { channel: 4, muted: getRandomBool() },
+      // 106 fm arp
+      { channel: 5, muted: getRandomBool() }
+    ];
+  } while (allChannelsMuted(mixer));
+  return mixer;
+}
+
+function allChannelsMuted(mixer) {
+  return mixer.reduce(
+    (allMuted, { muted }) => (!allMuted ? allMuted : muted),
+    true
+  );
+}
+
