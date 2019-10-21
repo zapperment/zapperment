be12b9a8889bb757edfef6316e41aaa1b1adac0a
Patrick Hund <pahund@team.mobile.de>
2019-10-21 09:00:58 +0200

    Optimized neural net configuration and increased max number of attempts
    Change-Id: If2a75ff868a2ecd2f33fb8e1e2ea9169cd08bc5a

--- packages/backend/src/model/train/trainNetwork.js
+++ packages/backend/src/model/train/trainNetwork.js
@@ -2,7 +2,7 @@ const brain = require("brain.js");
 const { normalize } = require("../normalize");
 
 module.exports = data => {
-  const net = new brain.NeuralNetwork();
+  const net = new brain.NeuralNetwork({ hiddenLayers: [5, 5] });
   const normalizedData = normalize(data);
   net.train(normalizedData);
   console.log(`NEURAL NETWORK TRAINED WITH ${normalizedData.length} LOOPS`);
