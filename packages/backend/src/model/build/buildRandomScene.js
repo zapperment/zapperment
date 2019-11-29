d7aff2d3869f6f31e1ebd7c9ac831490ed6ad2f4
Patrick Hund <pahund@team.mobile.de>
2019-11-29 09:58:23 +0100

    (squash me)
    Change-Id: I8fde4345cd8493fb47d25944ddd7fddf78fef644

--- packages/backend/src/model/build/buildRandomScene.js
+++ packages/backend/src/model/build/buildRandomScene.js
@@ -1,11 +1,44 @@
 const { getRandomBool } = require("./utils");
+const track = require("../../tracks/hamburgInAutumn");
 
-module.exports = () => ({
-  mixer: buildMixer(),
-  nervomat: {
-    annoyMe: getRandomBool()
-  }
-});
+function isButton(controlName) {
+  return controlName.startsWith("button");
+}
+
+function isRotary(controlName) {
+  return controlName.startsWith("rotary");
+}
+
+function buildRandomScene() {
+  return track.channels.map(({ name, elements, midi, controls }) => {
+    console.log("[PH_LOG] channel name:", name); // PH_TODO
+    const scene = {
+      midi: {
+        ...midi,
+        events: []
+      },
+      elements
+    };
+    Object.entries(controls).forEach(([controlName, { target, on, off }]) => {
+      console.log("[PH_LOG] name:", controlName); // PH_TODO
+      console.log("[PH_LOG] is button?", isButton(controlName)); // PH_TODO
+      if (isButton(controlName)) {
+        const isOn = getRandomBool();
+        const value = isOn ? 127 : 0;
+        scene.midi.events.push([{
+          // TODO add MIDI controller events here for combinator
+        }]);
+        // TODO: iterate values of controls and change scene elements accordingly
+      }
+    });
+    return scene;
+  });
+}
+
+module.exports = buildRandomScene;
+
+const scene = buildRandomScene();
+console.log(`[PH_LOG] scene\n${JSON.stringify(scene, null, 4)}`); // PH_TODO
 
 function buildMixer() {
   let mixer;
@@ -32,4 +65,3 @@ function allChannelsMuted(mixer) {
     true
   );
 }
-
