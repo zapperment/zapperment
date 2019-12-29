c7ab83ec6b927f6632c23d4cb31f5f4010e1619b
Patrick Hund <pahund@team.mobile.de>
2019-12-29 22:57:59 +0100

    Removed hidden layer config, Brain seems to work well with defaults
    Change-Id: I49d619dc54b9bb2e3586d9b18d20349ae7157abe

--- packages/backend/src/model/train/trainNetwork.js
+++ packages/backend/src/model/train/trainNetwork.js
@@ -3,7 +3,7 @@ const brain = require("brain.js");
 module.exports = normalizedData => {
   console.log(`Training neural network with ${normalizedData.length} loops`);
   const start = Date.now();
-  const net = new brain.NeuralNetwork({ hiddenLayers: [5, 5] });
+  const net = new brain.NeuralNetwork();
   net.train(normalizedData);
   console.log(`Done in ${Date.now() - start} ms`);
   return net.toFunction();
