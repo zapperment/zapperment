ea5a8779853247c9d1a0f425f6a0b9ac1e224863
Patrick Hund <pahund@team.mobile.de>
2020-02-27 11:09:06 +0100

    bumped version of Live MIDI remote script to 0.3.0
    Change-Id: I7f2ff17fa1e596ff73e524ca3fafa1a25d6388d4

--- packages/ableton-live/src/consts.py
+++ packages/ableton-live/src/consts.py
@@ -1,4 +1,4 @@
-software_version = "0.2.2_beta.8"
+software_version = "0.3.0"
 
 midi_note_on_offset = 0x90
 midi_note_off_offset = 0x80
@@ -9,4 +9,4 @@ midi_event_type_note_off = 'note off'
 midi_event_type_control = 'control'
 
 # In dev mode, logging is more verbose
-is_dev_mode = True
+is_dev_mode = False
