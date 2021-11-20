e6a166c7eb46782bd805f143be85ef406f83517e
Patrick Hund <pahund@team.mobile.de>
2021-11-20 21:40:18 +0100

    …squash me…

--- packages/x-toucher/src/CombinatorState/utils/getCombinatorStateProperty.js
+++ packages/x-toucher/src/CombinatorState/utils/getCombinatorStateProperty.js
@@ -1,7 +1,6 @@
-const debug = require("debug")("zapperment:x-toucher:get-device-state-property");
+// const debug = require("debug")("zapperment:x-toucher:get-device-state-property");
 
 function getCombinatorStateProperty(propertyName, data, previousDevice) {
-  debug(`Prop name: ${propertyName} – data: ${data[propertyName]} – prev dev: ${previousDevice[propertyName]}`);
   return data[propertyName] === undefined
     ? previousDevice[propertyName]
     : data[propertyName];
